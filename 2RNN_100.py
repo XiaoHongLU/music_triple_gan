849836bdc3ac54bdbd99f01c72f4d9277bdf3cf9
nlfox <nlfox@msn.cn>
2018-02-02 16:03:00 +0000

    2

--- 2RNN_100.py
+++ 2RNN_100.py
@@ -77,11 +77,11 @@ if not os.path.isfile(os.path.join(os.getcwd(),"Model/2RNN_100/2RNN_100.ckpt.met
         tf.logging.set_verbosity(tf.logging.DEBUG)
         tf.global_variables_initializer().run()
         tf.local_variables_initializer().run()
-        progress = open(os.path.join(os.getcwd(), 'Model/2RNN_100/2RNN_100.txt'), 'wb')
+        progress = open(os.path.join(os.getcwd(), 'Model/2RNN_100/2RNN_100.txt'), 'w')
         for step in range(EPOCH):
             feed_dict = {tf_train_data: data_train, tf_train_label: label_train}
             _, l, acc_train = session.run([optimizer, loss, acc], feed_dict=feed_dict)
-
+            print(step)
             if step % DISPLAY_EPOCH == 0:
                 loss_valid, predict_valid, acc_valid = session.run([loss, logits, acc], feed_dict={tf_train_data: data_valid,
                                                                                    tf_train_label: label_valid})
@@ -93,6 +93,7 @@ if not os.path.isfile(os.path.join(os.getcwd(),"Model/2RNN_100/2RNN_100.ckpt.met
                 progress.write('\n')
                 progress.write("step %d, validation : loss is %g" % (step, loss_valid))
                 progress.write('\n')
+                progress.flush()
 
         loss_test, predict_test = session.run([loss, logits],
                                          feed_dict={tf_train_data: data_test,
